22733ca9b49c628b709318b67c1ea1f392bf371b
aravind7lee <aravindhhh5@gmail.com>
2024-09-28 12:48:57 +0530

    Update
    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  // Include the Vite plugin for React
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),        // Alias '@' points to the 'src' folder
-      'context': path.resolve(__dirname, 'src/context')  // Alias 'context' points to the 'src/context' folder
+      '@': path.resolve(__dirname, 'src'),
+      'context': path.resolve(__dirname, 'src/context')  // Correctly set the alias
     }
   }
-})
+});
